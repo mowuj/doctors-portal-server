c00ef38d4d37d56b6e3b648e86448e046bb7b0db
mowuj <mowuj92@gmail.com>
2022-05-23 02:12:17 +0600

    recive booking data

--- index.js
+++ index.js
@@ -16,8 +16,29 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const servicesCollection = client.db('doctors_portal').collection('services');
-        
+        const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+
+     
+      app.get('/service', async (req, res) => {
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services)
+      });
+       /**
+       * API Naming Convention
+       * app.get('/booking')//get all booking in this collection od get more then one by filter
+       * app.get('/booking?:id')//get a specific booking
+       * app.get('/booking')//add a new booking
+       * app.patch('/booking/:id)//
+       * app.patch('/booking/:id)//
+       */
+      app.post('/booking', async (req, res) => {
+        const booking = req.body;
+        const result=await bookingCollection.insertOne(booking)
+        res.send(result)
+      })
     }
     finally {
         
