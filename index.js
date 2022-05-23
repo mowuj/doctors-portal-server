da3415e1933474e04e07dc81dbdeb5f602435c70
mowuj <mowuj92@gmail.com>
2022-05-23 13:30:48 +0600

    create jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
@@ -18,6 +19,7 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
+        const userCollection = client.db('doctors_portal').collection('users');
 
      
       app.get('/service', async (req, res) => {
@@ -26,6 +28,19 @@ async function run() {
         const services = await cursor.toArray();
         res.send(services)
       });
+      app.put('/user/:email',async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+      $set: user,
+        };
+        const result=await userCollection.updateOne(filter,updateDoc,options)
+        const token =jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        res.send({result,token});
+      })
+
       // Warning: This is not the proper way to query multiple collection. 
     // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
     app.get('/available', async(req, res) =>{
